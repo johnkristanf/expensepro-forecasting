c2649230d712b83797f425d8da0f1a703d786ff7
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2026-01-29 21:02:53 +0800

    feat: feature engineering with pandas

--- src/api/v1/forecasting.py
+++ src/api/v1/forecasting.py
@@ -1,5 +1,7 @@
 import decimal
 import holidays
+import pandas as pd
+
 from collections import defaultdict
 from datetime import datetime
 
@@ -18,7 +20,7 @@ forecast_router = APIRouter()
 @forecast_router.get("/")
 async def get_forecast_data(
     session: AsyncSession = Depends(Database.get_async_session),
-    forecasting_service: ForecastingService = Depends(get_forecasting_service)
+    forecasting_service: ForecastingService = Depends(get_forecasting_service),
 ):
     expenses_table = await reflect_existing_table("expenses")
     categories_table = await reflect_existing_table("categories")
@@ -30,8 +32,7 @@ async def get_forecast_data(
         )
         .select_from(
             expenses_table.join(
-                categories_table,
-                expenses_table.c.category_id == categories_table.c.id
+                categories_table, expenses_table.c.category_id == categories_table.c.id
             )
         )
         .where(categories_table.c.name == "Transportation")
@@ -41,22 +42,75 @@ async def get_forecast_data(
     result = await session.execute(join_stmt)
     rows = result.fetchall()
 
-    aggregation = defaultdict(float)
-    min_date = None
-    max_date = None
-    for row in rows:
-        amount_as_float = float(row.amount) if isinstance(row.amount, decimal.Decimal) else row.amount
-        date_value = row.date_spent.date() if isinstance(row.date_spent, datetime) else datetime.strptime(str(row.date_spent), "%Y-%m-%d").date()
+    import os
 
-        aggregation[date_value] += amount_as_float
-        if min_date is None or date_value < min_date:
-            min_date = date_value
-        if max_date is None or date_value > max_date:
-            max_date = date_value
+    df = pd.DataFrame(rows, columns=["date_spent", "amount"])
 
-    all_dates = forecasting_service.build_date_range(min_date, max_date)
+    df["date_spent"] = pd.to_datetime(df["date_spent"]).dt.date
+    df["amount"] = df["amount"].astype(float)
 
-    ph_holidays = holidays.country_holidays('PH')
-    data = forecasting_service.fill_date_gaps_with_properties(all_dates, aggregation, ph_holidays)
+    daily_expense = df.groupby("date_spent", as_index=False).agg(
+        total_amount=("amount", "sum")
+    )
 
-    return {"data": data}
+    full_date_range = pd.date_range(
+        start=daily_expense["date_spent"].min(),
+        end=daily_expense["date_spent"].max(),
+        freq="D",
+    )
+
+    # Fill missing dates
+    daily_expense = (
+        daily_expense.set_index("date_spent")
+        .reindex(full_date_range)
+        .rename_axis("date_spent")
+        .reset_index()
+    )
+
+    # Calendar Features
+    daily_expense["is_weekend"] = (daily_expense["date_spent"].dt.weekday == 6).astype(
+        int
+    )
+
+    ph_holidays = holidays.country_holidays(
+        country="PH", years=daily_expense["date_spent"].dt.year.unique()
+    )
+
+    date_as_date = daily_expense["date_spent"].dt.date
+    daily_expense["is_holiday"] = date_as_date.isin(ph_holidays).astype(int)
+
+
+    # Fill missing dates with 0 amount, indicating missing data -> no transportation or absent day, instead of unknown
+    daily_expense["total_amount"] = daily_expense["total_amount"].fillna(0.0)
+
+    daily_expense["is_absent_day"] = (
+        (daily_expense["is_weekend"] == 0)
+        & (daily_expense["is_holiday"] == 0)
+        & (daily_expense["total_amount"] == 0)
+    ).astype(int)
+
+    # Set IQR value to non-zeros to avoid large drop in 1st quantile
+    iqr_base = daily_expense[
+        (daily_expense["is_weekend"] == 0)
+        & (daily_expense["is_holiday"] == 0)
+        & (daily_expense["total_amount"] > 0)
+    ]
+
+    iqr_cap_amount = forecasting_service.get_iqr_capped_amount(iqr_base)
+    daily_expense["total_amount_capped"] = daily_expense["total_amount"].clip(
+        lower=0, upper=iqr_cap_amount
+    )
+    
+    # Optional features, for business explaination purposes
+    daily_expense["holiday_name"] = date_as_date.map(ph_holidays)
+
+    output_dir = "exports"
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, "transportation_forecast.csv")
+    daily_expense.to_csv(output_path, index=False)
+
+    return {
+        "message": "Export successful",
+        "path": output_path,
+        "iqr_cap_amount": iqr_cap_amount,
+    }
--- src/services/forecasting.py
+++ src/services/forecasting.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from datetime import datetime, timedelta
 
+from pandas import DataFrame
+
 
 class ForecastingService:
     def aggregate_expenses_by_date(self, rows):
@@ -12,8 +14,16 @@ class ForecastingService:
         min_date = None
         max_date = None
         for row in rows:
-            amount_as_float = float(row.amount) if isinstance(row.amount, (float, int)) else float(row.amount)
-            date_value = row.date_spent.date() if isinstance(row.date_spent, datetime) else datetime.strptime(str(row.date_spent), "%Y-%m-%d").date()
+            amount_as_float = (
+                float(row.amount)
+                if isinstance(row.amount, (float, int))
+                else float(row.amount)
+            )
+            date_value = (
+                row.date_spent.date()
+                if isinstance(row.date_spent, datetime)
+                else datetime.strptime(str(row.date_spent), "%Y-%m-%d").date()
+            )
 
             aggregation[date_value] += amount_as_float
             if min_date is None or date_value < min_date:
@@ -46,12 +56,23 @@ class ForecastingService:
             amount = aggregation.get(date, 0.0)
             is_weekend = 1 if date.weekday() == 6 else 0
             is_holiday = 1 if date in holidays_provider else 0
-            data.append({
-                "date": date,
-                "amount": amount,
-                "is_weekend": is_weekend,
-                "is_holiday": is_holiday
-            })
+            data.append(
+                {
+                    "date": date,
+                    "amount": amount,
+                    "is_weekend": is_weekend,
+                    "is_holiday": is_holiday,
+                }
+            )
         return data
-    
-    
+
+    def get_iqr_capped_amount(self, df: DataFrame):
+        Q1 = df["total_amount"].quantile(0.25)
+        Q3 = df["total_amount"].quantile(0.75)
+        IQR = Q3 - Q1
+        print("Q1: " + str(Q1))
+        print("Q3: " + str(Q3))
+        print("IQR: " + str(IQR))
+
+        cap_amount = Q3 + 1.5 * IQR
+        return cap_amount
